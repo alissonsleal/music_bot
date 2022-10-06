e02f913e6300afba9d56a27218946d922c64a689
Alisson Leal <alissonsleal@hotmail.com>
2022-10-05 22:38:16 -0300

    feat: create pause command

---
+++ src/commands/pause.ts
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+import { EmbedBuilder, Interaction } from 'discord.js'
+
+export const pause = {
+  data: new SlashCommandBuilder().setName('pause').setDescription('Pause the current playing song.'),
+  async execute(interaction: Interaction) {
+    if (!interaction.isCommand()) return
+
+    // @ts-ignore
+    const queue = interaction.client.player.getQueue(interaction.guildId)
+
+    if (!queue || !queue.playing) {
+      return interaction.reply({
+        content: 'No music is being played!',
+        ephemeral: true,
+      })
+    }
+
+    const success = queue.setPaused(true)
+
+    const currentTrack = queue.current
+
+    const successEmbed = new EmbedBuilder()
+      .setTitle('Paused song')
+      .setDescription(`[${currentTrack.title}](${currentTrack.url})`)
+      .setColor('#9e59ee')
+
+    const errorEmbed = new EmbedBuilder()
+      .setTitle('Error')
+      .setDescription('Could not pause the song.')
+      .setColor('#f33e3e')
+
+    return interaction.reply({
+      embeds: success ? [successEmbed] : [errorEmbed],
+    })
+  },
+}
